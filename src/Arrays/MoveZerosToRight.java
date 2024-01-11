75180f9f3b7bd70db1d46393109f3e83e1c26b5c
ianithkumar <anithani1206@gmail.com>
2024-01-11 21:36:18 +0530

    Completed MoveZerosToRight Without Using An Array

--- src/Arrays/MoveZerosToRight.java
+++ src/Arrays/MoveZerosToRight.java
@@ -3,6 +3,11 @@ package Arrays;
 public class MoveZerosToRight {
   public static void main(String a[]) {
     int arr[] = {0, 0, 2, 0, 3, 0, 4};
+    //moveZerosToRightWithArray(arr);
+    moveZerosToRightWithoutArray(arr);
+  }
+
+  private static void moveZerosToRightWithArray(int[] arr) {
     int result[] = new int[arr.length];
     int start = 0;
     int end = arr.length - 1;
@@ -19,4 +24,22 @@ public class MoveZerosToRight {
       System.out.print(result[j] + " ");
     }
   }
+
+  private static void moveZerosToRightWithoutArray(int[] arr) {
+    int count = 0;
+    int n = arr.length;
+    for (int i = 0; i < arr.length; i++) {
+      if (arr[i] != 0) {
+        arr[count++] = arr[i];
+      }
+    }
+    while (count < n) {
+      arr[count++] = 0;
+    }
+    for (int j = 0; j < arr.length; j++) {
+      System.out.print(arr[j] + " ");
+    }
+  }
 }
+
+
