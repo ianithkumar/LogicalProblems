af189a47605b484f591953ae30a11aa64581e30d
ianithkumar <anithani1206@gmail.com>
2024-03-24 10:52:24 +0530

    Added Some Problems

---
+++ src/LinkedList/ReverseLinkedList.java
@@ -0,0 +1,16 @@
+package LinkedList;
+
+import java.util.HashSet;
+import java.util.Set;
+import java.util.TreeSet;
+
+public class ReverseLinkedList {
+  public static void main(String a[]) {
+    Set<Integer> num = new TreeSet<>();
+    num.add(45);
+    num.add(56);
+    num.add(67);
+    num.add(89);
+    System.out.println(num);
+  }
+}
