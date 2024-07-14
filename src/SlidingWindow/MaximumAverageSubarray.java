f456a1b25f7a08e8ec5162de7bd36e0f5fa37faa
ianithkumar <anithani1206@gmail.com>
2024-07-14 12:53:54 +0530

    Updated Sliding Window

--- src/SlidingWindow/MaximumAverageSubarray.java
+++ src/SlidingWindow/MaximumAverageSubarray.java
@@ -1,2 +1,7 @@
-package SlidingWindow;public class MaximumAverageSubarray {
+package SlidingWindow;
+
+public class MaximumAverageSubarray {
+  public static void main(String a[]){
+    int[] nums = {};
+  }
 }
