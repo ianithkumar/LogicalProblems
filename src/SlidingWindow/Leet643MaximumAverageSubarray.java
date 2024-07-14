a37140b5fa472e356f5d1aca47ee383ba33ebe94
ianithkumar <anithani1206@gmail.com>
2024-07-14 19:24:56 +0530

    Updated Sliding Window

--- src/SlidingWindow/Leet643MaximumAverageSubarray.java
+++ src/SlidingWindow/Leet643MaximumAverageSubarray.java
@@ -1,6 +1,6 @@
 package SlidingWindow;
 
-public class MaximumAverageSubarray {
+public class Leet643MaximumAverageSubarray {
   public static void main(String a[]){
     int[] nums = {1,12,-5,-6,50,3};
     int k = 4;                         //Output = 12.75
