d9729f67acce0613e2b8b96a090a6bea003f89ea
ianithkumar <anithani1206@gmail.com>
2024-07-27 11:45:51 +0530

    Completed a Problem in BFS

--- src/main/java/BFS/BinaryTreeLevelOrderTraversal.java
+++ src/main/java/BFS/BinaryTreeLevelOrderTraversal.java
@@ -1,14 +1,11 @@
 package BFS;
 
 
-import java.util.ArrayDeque;
-import java.util.ArrayList;
-import java.util.List;
-import java.util.Queue;
+import java.util.*;
 
-//[3,9,20,null,null,15,17]
+//Input : [3,9,20,null,null,15,17]   Output : [[3], [9, 20], [15, 17]]
 
-public class LevelOrderTraversal {
+public class BinaryTreeLevelOrderTraversal {
   public static void main(String[] a) {
     TreeNode head = new TreeNode(3);
     head.left = new TreeNode(9);
@@ -19,16 +16,27 @@ public class LevelOrderTraversal {
 
   private static List<List<Integer>> levelOrder(TreeNode head) {
     List<List<Integer>> bigList = new ArrayList<>();
-    Queue<Integer> queue = new ArrayDeque<>();
-    queue.add(head.val);
+    Queue<TreeNode> queue = new ArrayDeque<>();
+
+    if (head == null) {
+      return bigList;
+    }
+    queue.offer(head);
 
     while (queue.size() > 0) {
       int size = queue.size();
       List<Integer> smallList = new ArrayList<>();
 
       for (int i = 0; i < size; i++) {
-        int node = queue.poll();
-        smallList.add(node);
+        TreeNode node = queue.poll();
+        smallList.add(node.val);
+
+        if (node.left != null) {
+          queue.offer(node.left);
+        }
+        if (node.right != null) {
+          queue.offer(node.right);
+        }
 
 
       }
