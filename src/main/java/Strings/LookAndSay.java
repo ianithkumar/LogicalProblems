473d791654bc83fb1ab4286e707e72b495631d5e
ianithkumar <anithani1206@gmail.com>
2024-08-01 19:25:00 +0530

    Updated Dynamic Programming

--- src/main/java/Strings/LookAndSay.java
+++ src/main/java/Strings/LookAndSay.java
@@ -1,7 +1,30 @@
 package Strings;
 
 public class LookAndSay {
-  public static void main(String[] a){
-
+  public static void main(String[] a) {
+    String startNum = "1";
+    int endNum = 4;
+    for (int i = 1; i <= endNum; i++) {
+      startNum = generateNextTerm(startNum);
+    }
+    System.out.println(startNum);
   }
+
+  private static String generateNextTerm(String num) {
+    //String result = "";
+    StringBuilder result = new StringBuilder();
+    int count = 1;
+
+    for (int i = 1; i < num.length(); i++) {
+      if (num.charAt(i) == num.charAt(i - 1)) {
+        count++;
+      } else {
+        //result = result+count+num.charAt(i-1);
+        result.append(count).append(num.charAt(i - 1));
+        count = 1;
+      }
+    }
+    return result.toString();
+  }
+
 }
