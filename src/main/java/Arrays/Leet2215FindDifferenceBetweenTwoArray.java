515bba2d75fd31d31682c645e7371e0d3d169a5d
ianithkumar <anithani1206@gmail.com>
2024-08-02 19:27:13 +0530

    Updated Arrays Package

--- src/main/java/Arrays/Leet2215FindDifferenceBetweenTwoArray.java
+++ src/main/java/Arrays/Leet2215FindDifferenceBetweenTwoArray.java
@@ -1,36 +1,47 @@
 package Arrays;
 
-import java.util.*;
+import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
 
 public class Leet2215FindDifferenceBetweenTwoArray {
-  public static void main(String a[]){
-    int nums1[] = {1,2,3};
-    int nums2[] = {2,4,6};
-    List<List<Integer>> result = findDifference(nums1,nums2);
+  public static void main(String[] arr) {
+    int nums1[] = {1, 2, 3};
+    int nums2[] = {2, 4, 6};
+    List<List<Integer>> result = findDifference(nums1, nums2);
     System.out.println(result);
   }
-  public static List<List<Integer>> findDifference(int[] nums1, int[] nums2) {
+
+  private static List<List<Integer>> findDifference(int[] nums1, int[] nums2) {
     List<List<Integer>> result = new ArrayList<>();
     Set<Integer> set1 = new HashSet<>();
-    for(int i=0;i<nums1.length;i++){
-      for(int j=0;j<nums2.length;j++){
-        if(nums1[i]!=nums2[j]){
-          set1.add(nums1[i]);
-        }
-      }
+    for (int num : nums1) {
+      set1.add(num);
     }
-    List<Integer> arr1 = new ArrayList<>(set1);
-    result.add(arr1);
+
     Set<Integer> set2 = new HashSet<>();
-    for(int i=0;i<nums1.length;i++){
-      for(int j=0;j<nums2.length;j++){
-        if(nums2[i]!=nums1[j]){
-          set2.add(nums2[i]);
-        }
+    for (int num : nums2) {
+      set2.add(num);
+    }
+    List<Integer> list1 = new ArrayList<>();
+
+    for (int num : set1) {
+      if (!set2.contains(num)) {
+        list1.add(num);
       }
     }
-    List<Integer> arr2 = new ArrayList<>(set2);
-    result.add(arr2);
+
+    List<Integer> list2 = new ArrayList<>();
+
+    for (int num : set2) {
+      if (!set1.contains(num)) {
+        list2.add(num);
+      }
+    }
+
+    result.add(list1);
+    result.add(list2);
     return result;
   }
 }
