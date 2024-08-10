7feb2dd00cd4a8f5168e2d7a1242ac85423cd9b0
ianithkumar <anithani1206@gmail.com>
2024-08-10 18:54:34 +0530

    Updated Array Package

--- src/main/java/Arrays/RotateArrayWithMValue.java
+++ src/main/java/Arrays/RotateArrayWithMValue.java
@@ -1,19 +1,19 @@
 package Arrays;
 
 public class RotateArrayWithMValue {
-  public static void main(String[] a){
-    int[] arr = {1,2,3,4,5,6};
-    int M = 2;
+  public static void main(String[] a) {
+    int[] arr = {1, 2, 3, 4, 5, 6, 7};
+    int M = 3;
     System.out.println("Before Rotation : ");
-    for(int num:arr){
-      System.out.print(num+" ");
+    for (int num : arr) {
+      System.out.print(num + " ");
     }
 
-    for(int i=0;i<arr.length;i=i+M){
+    for (int i = 0; i < arr.length; i = i + M) {
       int start = i;
-      int end = start+M-1;
-      if(end<arr.length-1){
-        while(start<end){
+      int end = start + M - 1;
+      if (end < arr.length) {
+        while (start < end) {
           int temp = arr[start];
           arr[start] = arr[end];
           arr[end] = temp;
@@ -24,8 +24,8 @@ public class RotateArrayWithMValue {
     }
     System.out.println();
     System.out.println("After Rotation : ");
-    for(int num:arr){
-      System.out.print(num+" ");
+    for (int num : arr) {
+      System.out.print(num + " ");
     }
   }
 }
